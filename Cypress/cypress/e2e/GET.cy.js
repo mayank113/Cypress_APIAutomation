631bbba6dc2cea8614062e26f052e8fea3bab2ca
Mayank113 <mayank.saxena12051990@gmail.com>
2024-05-08 12:37:37 +0530

    first cypress commit

---
+++ Cypress/cypress/e2e/GET.cy.js
@@ -0,0 +1,33 @@
+/// <refrence types = "Cypress" />
+
+describe('get api user', () => {
+
+  let accessToken = "ca5bc36b2f935ea61824051239a9fcff74b3045312238460b9938a03dd96b372" 
+
+  it('passes', () => {
+    cy.request({
+method : "GET",
+url : "https://dummyjson.com/products"
+
+    }).then((res)=>{
+
+      expect(res.status).to.eq(200)
+    })
+  })
+
+  it('passes', () => {
+    cy.request({
+    method : "GET",
+    url : "https://gorest.co.in/public/v2/users",
+    Headers : {
+              'authorization' : "Bearer " + accessToken
+    }
+
+    }).then((res)=>{
+
+      expect(res.status).to.eq(200)
+    })
+  })
+
+
+})
