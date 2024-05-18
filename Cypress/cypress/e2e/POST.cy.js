2e2b34804f9cdb3117f4e1bfdf5a1652d4f88f4f
mayank113 <34433709+mayank113@users.noreply.github.com>
2024-05-18 23:06:03 +0530

    Update POST.cy.js
    Updating Post request

--- Cypress/cypress/e2e/POST.cy.js
+++ Cypress/cypress/e2e/POST.cy.js
@@ -1,9 +1,12 @@
 /// <refrence types = "Cypress" />
 describe('Post User create',() =>{
-    let accessToken = 'd27cfd2572ad5271a9e0f3ace385a793ef9b744721cddf8d10a6cb8be331cc4c'
+    let accessToken = 'b36af3df2be8b313967328ae6120e97366af49a77255cf5b607b833cfba98dd0'
     let Email = ""
     let RandomText = ""
-    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const characters ='d27cfd2572ad5271a9e0f3ace385a793ef9b744721cddf8d10a6cb8be331cc4c'
+    let userid= '' 
+
+
     //Random Email Generation
     for ( let i = 0; i < 10; i++ ) 
         RandomText += characters.charAt(Math.floor(Math.random() * characters.length));
@@ -34,23 +37,26 @@ it('Create User', () => {
       expect(res.body).has.property('status',datajson.status)
       expect(res.body).has.property('gender',datajson.gender)
       expect(res.body).has.property('email',Email)
-    }).then((res) =>{
-//get user details created
-    let userid = 6896061
-    cy.log("User ID", userid)
+      userid = res.body.id
+      cy.log("User ID Created: "+userid)
+    })
     cy.request({
         method : "GET",
         url : 'https://gorest.co.in/public/v2/users/'+ userid,
         headers : {
                   'authorization' : "Bearer " + accessToken
         },  
-    }).then((res)=>{
+    }).then((res1)=>{
 
-        expect(res.status).to.eq(200)
+        expect(res1.status).to.eq(200)
+        cy.log("ID: " + res1.body[0].id)
+        cy.log("Name: " + res1.body[0].name)
+        cy.log("Gender: " + res1.body[0].gender)
+        cy.log("Email: " + res1.body[0].email)
+        cy.log("Status: " + res1.body[0].status)
 
     })
 
     })
 
   })
-})
